39f9ef610e122382ae16e1d9704566e90bd1bd47
abu <abusayed310278@gmail.com>
2024-11-07 04:09:17 +0600

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-
+import { Header } from "./components/Header";
+import { TaskList } from "./components/TaskList";
 import './App.css';
-import {Header} from "./components/Header";
-import {TaskList} from "./components/TaskList";
-
-
 
 function App() {
-
-
-
-
 
     return (
         <div className="App">
             <Header />
-
-            <TaskList title="Random"/>
-
+            <TaskList />
         </div>
-    )
+    );
 }
-
 
 export default App;
--- src/components/TaskCard.js
+++ src/components/TaskCard.js
@@ -1,9 +1,8 @@
-export const TaskCard=(props)=>{
-
-    return(
-        <li key={props.task.id}>
-            <span>{props.task.id}-{props.task.name}</span>
-            <button onClick={()=>props.handleDelete(props.task.id)} className='delete'>Delete</button>
+export const TaskCard = ({ task, handleDelete }) => {
+    return (
+        <li className={task.completed ? "completed" : "incomplete"}>
+            <span>{task.id} - {task.name}</span>
+            <button onClick={() => handleDelete(task.id)} className='delete'>Delete</button>
         </li>
     )
 }
