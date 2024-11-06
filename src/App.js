5868704052d0d22c16b833542db2a0f470042e6c
abu <abusayed310278@gmail.com>
2024-11-07 00:22:15 +0600

    conditional template

--- src/App.js
+++ src/App.js
@@ -1,42 +1,39 @@
-import {Header} from "./components/Header"
-import {Footer} from "./components/Footer"
 import {useState} from 'react'
 import './App.css';
 
 
+
 function App() {
 
+    const [tasks,setTasks]=useState([
+        {id:1,name:'record 1',completed:true},
+        {id:2,name:'record 2',completed:false},
+        {id:3,name:'record 3',completed:false},
+    ]);
 
-    const [count,setCount]=useState(0)
+    const [show,setShow]=useState(false)
 
-
-    function handleAdd() {
-        // setCount(count+1)
-        // setCount(count+1)
-        // setCount(count+1)
-
-        setCount(count=>count+1)
-        setCount(count=>count+1)
-        setCount(count=>count+1)
+    function handleDelete(id){
+        setTasks(tasks.filter(task=>task.id!==id))
     }
 
-    function handleSub() {
-        setCount(count-1)
-    }
-
-    function handleReset() {
-        setCount(0)
-    }
 
 
     return (
         <div className="App">
-            <div className="box">
-                <p>{count}</p>
-                <button onClick={()=>setCount(count+1)} className="add">Add</button>
-                <button onClick={()=>setCount(count-1)} className="sub">sub</button>
-                <button onClick={()=>setCount(0)} className="reset">RESET</button>
-            </div>
+            <h1>Task List</h1>
+            <ul>
+                <button onClick={()=>setShow(!show)}>Toggle</button>
+                {
+                   show && tasks.map((task)=>(
+                        <li key={task.id}>
+                            <span>{task.id}-{task.name}</span>
+                            <button onClick={()=>handleDelete(task.id)} className='delete'>Delete</button>
+                        </li>
+                    ))
+                }
+
+            </ul>
         </div>
     )
 }
