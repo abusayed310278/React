79192c97158c2e9df5bd575b794576141d8009c7
abu <abusayed310278@gmail.com>
2024-11-07 21:06:13 +0600

    useCallback

--- 032/src/components/ProductList.js
+++ 032/src/components/ProductList.js
@@ -2,30 +2,39 @@ import { useEffect, useState } from "react"
 
 export const ProductList = () => {
     const [products, setProducts] = useState([]);
-    console.log(products);
-    
+    const [url,setUrl]=useState("http://localhost:3001/products")
+
     useEffect(() => {
-        fetch("http://localhost:3001/products")
-        .then(response => response.json())
-        .then(data => setProducts(data));
-    }, []);
 
-  return (
-    <section>
-        {
-            products.map((product)=>(
+        const fetchProducts=async ()=>{
+            const res=await fetch(url);
+            const data=await res.json();
+            setProducts(data);
+        }
+
+        fetchProducts();
+        // console.log("1")
+
+    }, [url]);
+
+    return (
+        <section>
+            <div className="filter">
+            <button onClick={()=>setUrl("http://localhost:3001/products")}>All</button>
+            <button onClick={()=>setUrl("http://localhost:3001/products?in_stock=true")}>In Stock</button>
+
+            </div>
+
+            { products.map((product) => (
                 <div className="card" key={product.id}>
-
                     <p className="id">{product.id}</p>
                     <p className="name">{product.name}</p>
                     <p className="info">
                         <span>${product.price}</span>
-                        <span className={product.in_stock?"instock":"unavailable"}>{product.in_stock}</span>
+                        <span className={product.in_stock ? "instock" : "unavailable"}>{product.in_stock ? "In Stock" : "Unavailable"}</span>
                     </p>
-
                 </div>
-            ))
-        }
-    </section>
-  )
+            )) }
+        </section>
+    )
 }
